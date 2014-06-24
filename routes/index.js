5a166ab1bba43dc6e2d9d7a8b1b66616ba5694c0
Chris Bartley <bartley@cmu.edu>
2014-06-24 17:47:12 -0400

    Factored out header and footer into Handlebars partials.  Stubbed out the Public Data, About, and Contact pages.

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,27 @@ var passport = require('passport');
 var log = require('log4js').getLogger();
 
 router.get('/', function(req, res) {
-   res.render('index', { title : "cattfish.com"});
+   res.render('index', { title : "CATTfish", section: "home"});
+});
+
+router.get('/about', function(req, res) {
+   res.render('about', { title : "CATTfish: About", section: "about"});
+});
+
+router.get('/contact', function(req, res) {
+   res.render('contact', { title : "CATTfish: Contact", section: "contact"});
+});
+
+router.get('/data', function(req, res) {
+   res.render('data', { title : "CATTfish: Data", section: "data"});
 });
 
 router.get('/register', function(req, res) {
-   res.render('register', { title : "Register"});
+   res.render('register', { title : "CATTfish: Register"});
 });
 
 router.get('/login', function(req, res) {
-   res.render('login', { title : 'Login' });
+   res.render('login', { title : 'CATTfish: Login' });
 });
 
 router.post('/login', passport.authenticate('local',
