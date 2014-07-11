2d3153fb9c23d1b6c58a3f639553374ec5df2add
Chris Bartley <bartley@cmu.edu>
2014-07-11 18:05:46 -0400

    Added login popup, implemented validation and error feedback.  Login works well now.

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,65 +6,68 @@ var config = require('../config');
 
 module.exports = function(UserModel) {
 
-   passport.use(new LocalStrategy(
-         function(username, password, done) {
-            var url = config.get("oauth:tokenUrl");
-            log.debug("Oauth to ESDR for login of user [" + username + "]");
-            request.post(url, {
-               form : {
-                  grant_type : 'password',
-                  client_id : config.get("oauth:clientId"),
-                  client_secret : config.get("oauth:clientSecret"),
-                  username : username,
-                  password : password
-               }
-            }, function(error, response, body) {
-               if (error) {
-                  log.debug("   ESDR oauth failed: " + error);
-                  return done(error, false);
-               }
+   passport.use(new LocalStrategy({
+                                     usernameField : 'email',
+                                     passwordField : 'password'
+                                  },
+                                  function(username, password, done) {
+                                     log.debug("Oauth to ESDR for login of user [" + username + "]");
+                                     var url = config.get("oauth:tokenUrl");
+                                     request.post(url, {
+                                        form : {
+                                           grant_type : 'password',
+                                           client_id : config.get("oauth:clientId"),
+                                           client_secret : config.get("oauth:clientSecret"),
+                                           username : username,
+                                           password : password
+                                        }
+                                     }, function(error, response, body) {
+                                        if (error) {
+                                           log.debug("   ESDR oauth failed: " + error);
+                                           return done(error, false);
+                                        }
 
-               try {
-                  var jsonResponse = JSON.parse(body);
-                  if (response.statusCode === 200 && jsonResponse.access_token) {
-                     var tokens = jsonResponse;
-                     log.debug("   ESDR oauth for user [" + username + "] successful!");
-                     var user = {
-                        username : username,
-                        lastLogin: new Date(),
-                        accessToken : tokens.access_token,
-                        refreshToken : tokens.refresh_token,
-                        accessTokenExpiration : new Date(new Date().getTime() + (tokens.expires_in * 1000))
-                     };
-                     UserModel.createOrUpdateTokens(user, function(err, result) {
-                        if (err) {
-                           return done(err, false);
-                        }
-                        // need to add the ID so we can store it in the session
-                        user.id = result.insertId;
-                        return done(null, user);
-                     });
-                  }
-                  else if (response.statusCode === 403) {
-                     log.debug("   ESDR oauth for user [" + username + "] failed.");
-                     return done(null, false, {
-                        message : 'Login failed.',
-                        error : jsonResponse.error || "unknown error",
-                        error_description : jsonResponse.error_description || "unknown error while authenticating user " + username
-                     });
-                  }
-                  else {
-                     log.error("   ESDR oauth for user [" + username + "] failed due to unknown error.  HTTP status [" + response.statusCode + "]");
-                     return done(null, false, { message : 'Unknown error, HTTP status [' + response.statusCode + ']' });
-                  }
-               }
-               catch (e) {
-                  log.error("   Unexpected exception while trying to authenticate user [" + username + "] with ESDR: " + e);
-                  return done(null, false, { message : 'Unexpected error, please try again later.' }); // TODO: should this be null and false?
-               }
+                                        try {
+                                           var jsonResponse = JSON.parse(body);
+                                           if (response.statusCode === 200 && jsonResponse.access_token) {
+                                              var tokens = jsonResponse;
+                                              log.debug("   ESDR oauth for user [" + username + "] successful!");
+                                              var user = {
+                                                 username : username,
+                                                 lastLogin : new Date(),
+                                                 accessToken : tokens.access_token,
+                                                 refreshToken : tokens.refresh_token,
+                                                 accessTokenExpiration : new Date(new Date().getTime() + (tokens.expires_in * 1000))
+                                              };
+                                              UserModel.createOrUpdateTokens(user, function(err, result) {
+                                                 if (err) {
+                                                    return done(err, false);
+                                                 }
+                                                 // need to add the ID so we can store it in the session
+                                                 user.id = result.insertId;
+                                                 return done(null, user);
+                                              });
+                                           }
+                                           else if (response.statusCode === 403) {
+                                              log.debug("   ESDR oauth for user [" + username + "] failed.");
+                                              return done(null, false, {
+                                                 message : 'Login failed.',
+                                                 error : jsonResponse.error || "unknown error",
+                                                 error_description : jsonResponse.error_description || "unknown error while authenticating user " + username
+                                              });
+                                           }
+                                           else {
+                                              log.error("   ESDR oauth for user [" + username + "] failed due to unknown error.  HTTP status [" + response.statusCode + "]");
+                                              return done(null, false, { message : 'Unknown error, HTTP status [' + response.statusCode + ']' });
+                                           }
+                                        }
+                                        catch (e) {
+                                           log.error("   Unexpected exception while trying to authenticate user [" + username + "] with ESDR: " + e);
+                                           return done(null, false, { message : 'Unexpected error, please try again later.' }); // TODO: should this be null and false?
+                                        }
 
-            });
-         }
+                                     });
+                                  }
    ));
 
    passport.serializeUser(function(user, done) {
