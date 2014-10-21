0d2c9904f9326a8751c1018f9dcc66325d2c756d
Chris Bartley <bartley@cmu.edu>
2014-10-21 17:27:29 -0400

    Devices are now created with a default name

--- public/js/handlebars_templates.js
+++ public/js/handlebars_templates.js
@@ -5,7 +5,7 @@ templates['device_list_item'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main
   return "<div id=\"device_list_item_"
     + escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"id","hash":{},"data":data}) : helper)))
     + "\" class=\"device_list_item noselect\">\n   <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">\n      <tr valign=\"middle\">\n         <td>\n            <div>"
-    + escapeExpression(((helper = (helper = helpers.serialNumber || (depth0 != null ? depth0.serialNumber : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"serialNumber","hash":{},"data":data}) : helper)))
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
     + "</div>\n            <div>Created: "
     + escapeExpression(((helper = (helper = helpers.created || (depth0 != null ? depth0.created : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"created","hash":{},"data":data}) : helper)))
     + "</div>\n         </td>\n      </tr>\n   </table>\n</div>";
